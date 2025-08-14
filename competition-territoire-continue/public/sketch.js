7a72031d7b103c7defa4c5846c1c8b748869628b
poum6502 <mathilde.poulin@usherbrooke.ca>
2025-08-14 15:24:07 -0400

    Ajout de l'initialisation

--- competition-territoire-continue/public/sketch.js
+++ competition-territoire-continue/public/sketch.js
@@ -1,9 +1,69 @@
+let lignes;
+let colonnes;
+cellules = [];
+
+initialisationFaite = false;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+
+  socket = io.connect('http://localhost:3000');
+  socket.on('initialisation', recevoirInitinalisation);
+  socket.on('grille', recevoirGrille);
 }
 
 function draw() {
-  background(255,50,200);
-  noStroke();
-  ellipse(mouseX, mouseY, 50, 50);
+  background(220, 100, 200);
+
+  if (initialisationFaite) {
+  afficherGrille();
+  }
+}
+
+function mousePressed() {
+  let position = {
+    x: mouseX,
+    y: mouseY
+  }
+
+  socket.emit('modificationGrille', position);
+}
+
+function recevoirGrille(cellulesServeur) {
+  cellules = Array.from(cellulesServeur);
+}
+
+function recevoirInitinalisation(dataInit) {
+    cellules = Array.from(dataInit.grille);
+    lignes = dataInit.lignes;
+    colonnes = dataInit.colonnes;
+
+    print('colonnes' + colonnes);
+    print('lignes' + lignes);
+
+    initialisationFaite = true;
+}
+
+function afficherGrille() {
+  stroke(192);
+  strokeWeight(1);
+
+  let tailleCellule = 20;
+
+  for (let i = 0; i < colonnes; i++) {
+    for (let j = 0; j < lignes; j++) {
+      switch (cellules[i][j]) {
+        case 0:
+          fill(32);
+          break;
+        case 1:
+          fill(255, 100, 100);
+          break;
+        case 2:
+          fill(100, 100, 255);
+          break;
+      }
+      square(i * tailleCellule, j * tailleCellule, tailleCellule);
+    }
+  }
 }
--- competition-territoire-continue/server.js
+++ competition-territoire-continue/server.js
@@ -1,3 +1,25 @@
+let colonnes = 288;
+let lignes = 62;
+let cellules = Array.from({ length: colonnes }, () => Array(lignes).fill(0));
+
+let joueursId = [];
+
+// let tempsIteration = 200;
+
+for (let i = 0; i < colonnes; i++) {
+    for (let j = 0; j < lignes; j++) {
+        if (Math.random() <= 0.4) {
+            if (Math.random() <= 0.5) {
+                cellules[i][j] = 1;
+            } else {
+                cellules[i][j] = 2;
+            }
+        } else {
+            cellules[i][j] = 0;
+        }
+    }
+}
+
 // Importation du module Express
 let express = require('express');
 
@@ -8,4 +30,79 @@ app.use(express.static('public'));
 console.log("Serveur opérationnel");
 let socket = require('socket.io');
 let io = socket(server);
-// io.sockets.on('connection', nouvelleConnexion);
+io.sockets.on('connection', nouvelleConnexion);
+
+function nouvelleConnexion(socket) {
+    joueursId.push(socket.id);
+
+    console.log('Nouvelle connexion ' + socket.id);
+
+    dataInit = {
+        'grille' : cellules,
+        'colonnes' : colonnes,
+        'lignes' : lignes
+    }
+
+    io.sockets.emit('initialisation', dataInit);
+
+    socket.on('modificationGrille', modificationGrille);
+
+    function modificationGrille(position) {
+        let joueurId = joueursId.indexOf(socket.id);
+        console.log(position);
+        console.log("Socket : " + socket.id + " (joueur " + (joueurId + 1) + ")");
+        let cellX = Math.floor(position.x / 20);
+        let cellY = Math.floor(position.y / 20);
+        if (cellX >= 0 && cellX < colonnes && cellY >= 0 && cellY < lignes) {
+            cellules[cellX][cellY] = joueurId + 1;
+        }
+
+        io.sockets.emit('grille', cellules);
+        console.log(cellules);
+    }
+}
+
+// Simulation d'une nouvelle génération
+function calculerGenerationSuivante() {
+    // Initialiser toutes les cellules à 0
+    let grilleGenerationSuivante = Array.from({ length: colonnes }, () => Array(lignes).fill(0));
+
+    for (let i = 0; i < colonnes; i++) {
+        for (let j = 0; j < lignes; j++) {
+            let etatsVoisins = obtenirEtatsVoisins(i, j);
+            const nbVoisinesVivantes = etatsVoisins.length;
+            const etatActuel = cellules[i][j];
+
+            // Naissance
+            if (etatActuel === 0 && (nbVoisinesVivantes === 3)) {
+                grilleGenerationSuivante[i][j] = Math.floor(1 + Math.random() * 2);
+            }
+
+            // Survie
+            if (etatActuel >= 1 && (nbVoisinesVivantes === 2 || nbVoisinesVivantes === 3)) {
+                grilleGenerationSuivante[i][j] = etatActuel;
+            }
+        }
+    }
+    cellules = Array.from(grilleGenerationSuivante);
+    io.sockets.emit('grille', cellules);
+}
+
+function obtenirEtatsVoisins(i, j) {
+    const voisins = [];
+    for (let k = -1; k <= 1; k++) {
+        for (let l = -1; l <= 1; l++) {
+            if (k === 0 && l === 0) continue;
+            const ni = (i + k + lignes) % lignes;
+            const nj = (j + l + colonnes) % colonnes;
+
+            const etat = cellules[ni][nj];
+            if (etat >= 1) {
+                voisins.push(etat);
+            }
+        }
+    }
+    return voisins;
+}
+
+setInterval(calculerGenerationSuivante, 1000);
